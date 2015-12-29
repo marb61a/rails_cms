2631519307e5cc88f18696daf60e574831b44345
marb <marb61a@gmail.com>
2015-12-29 17:54:38 +0000

    updated non admin controllers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,15 @@
 class CategoriesController < ContentController
-    def show
-        @category = Category.includes(:pages).find params[:id]
+  def show
+    @category = Category.includes(:pages).find params[:id]
+    render template: "category"
+  end
 
-    end
-    
+  protected
+      def template_to_render
+        if template_exists?("category-#{@category.id}")
+          "category-#{@category.id}"
+        else
+          "category"
+        end
+      end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,17 @@
-class PagesController < ApplicationController
-    def show
-        
-    end
+class PagesController < ContentController
+  def show
+    @page = Page.find params[:id]
+    render template: template_to_render
+  end
+
+  protected
+      def template_to_render
+        if template_exists?("page-#{@page.id}")
+          "page-#{@page.id}"
+        elsif template_exists? "page-#{@page.type.name}"
+          "page-#{@page.type.name}"
+        else
+          "page"
+        end
+      end
 end
